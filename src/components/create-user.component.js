a689640e55d8fec43efa0b0b78c398851f42cee6
anushreejain98 <anushreejain98@gmail.com>
2020-03-06 13:15:40 +0530

    uploaded initial project MERN Exercise tracker

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React,{ Component} from 'react';
+
+export default class CreateUsers extends Component{
+    render(){
+        return(
+            <div>
+                <p>You are on CreateUsers Component </p>
+            </div>
+        )
+    }
+}
